68ae518ce3dd00fab61b3def6e3263628356fba0
calmw <calm.wang@hotmail.com>
2023-09-25 17:14:28 +0800

    测试

---
+++ contracts/Swap.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./RoleAccess.sol";
+import "./interface/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+
+contract Swap is RoleAccess, Initializable {
+    using Address for address payable;
+
+    event SwapRecord(
+        address user,
+        uint256 toxNum,
+        uint256 matNum,
+        uint256 ctime
+    );
+
+    // TOX合约地址
+    address public toxAddress;
+    // 每次兑换，最多使用的TOX数量
+    uint256 public maxToxPerTime; // 每次兑换，最多使用的TOX数量
+    // 每次兑换，最少使用的TOX数量
+    uint256 public minToxPerTime; // 每次兑换，最少使用的TOX数量
+    // 兑换汇率
+    uint256 public rate; // 兑换汇率
+    // 用户，最最多可使用的TOX数量
+    uint256 public swapTotalMax; // 每个用户兑换，最最多可使用的TOX数量
+    // 用户兑换累计使用的TOX数量
+    mapping(address => uint256)  public userSwapTotal; // 用户兑换累计使用的TOX数量
+
+    function initialize() public initializer {
+        _addAdmin(msg.sender);
+    }
+
+    // 管理员设置每次兑换，最多使用的TOX数量
+    function adminSetToxAddress(address toxAddress_) public onlyAdmin {
+        toxAddress = toxAddress_;
+    }
+
+    // 管理员设置每次兑换，最多使用的TOX数量
+    function adminSetMaxToxPerTime(uint256 maxToxPerTime_) public onlyAdmin {
+        maxToxPerTime = maxToxPerTime_;
+    }
+
+    // 管理员设置每次兑换，最多使用的TOX数量
+    function adminSetMinToxPerTime(uint256 minToxPerTime_) public onlyAdmin {
+        minToxPerTime = minToxPerTime_;
+    }
+
+    // 管理员设置兑换汇率
+    function adminSetRate(uint256 rate_) public onlyAdmin {
+        rate = rate_;
+    }
+
+    // 管理员设置用户，最最多可使用的TOX数量
+    function adminSetSwapTotalMax(uint256 swapTotalMax_) public onlyAdmin {
+        swapTotalMax = swapTotalMax_;
+    }
+
+    // 兑换
+    function swap(uint256 toxNum_) external {
+        require(toxNum_ >= minToxPerTime, "TOX is too less");
+        require(toxNum_ <= maxToxPerTime, "TOX is too much");
+        require(userSwapTotal[msg.sender] + toxNum_ <= swapTotalMax, "the quantity you redeemed exceeds the limit");
+
+        IERC20 TOXContract = IERC20(toxAddress);
+        uint256 userBalance = TOXContract.balanceOf(msg.sender);
+        require(userBalance >= toxNum_, "your balance is insufficient");
+        TOXContract.transferFrom(msg.sender, address(this), toxNum_);
+        userSwapTotal[msg.sender] += toxNum_;
+        uint256 matNum = toxNum_ / rate;
+        address payable sender = payable(msg.sender);
+        sender.sendValue(matNum);
+    }
+
+    // 获取当前合约余额
+    function getMatBalance() external view onlyAdmin returns (uint256){
+        return address(this).balance;
+    }
+
+    // 提现
+    function widthDrawMat(address user_, uint256 amount_) external onlyAdmin {
+        address payable user = payable(user_);
+        user.sendValue(amount_);
+    }
+
+    receive() external payable {}
+}
